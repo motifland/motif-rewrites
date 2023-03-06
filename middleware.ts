52e224bf1f33e1404009d809b06d5735a330e117
Michael Fester <michael.fester@gmail.com>
2023-03-05 19:18:42 -0800

    Wip

--- middleware.ts
+++ middleware.ts
@@ -7,10 +7,15 @@ export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
 
   if (pathname.startsWith('/docs')) {
+    // Rewrite /docs... to external.
     return NextResponse.rewrite(MOTIF_PROJECT_URL + pathname)
   } else if (pathname.startsWith('/__motif/assets/_next/')) {
+    // Any assets coming in from external have path
+    // /__motif/assets/_next/... as per "assetPrefix", so
+    // rewrite accordingly.
     return NextResponse.rewrite(MOTIF_PROJECT_URL + pathname)
   } else if (pathname.startsWith('/_next/data/')) {
+    // Rewrite /_next/data/<buildId>/docs... to external.
     const match = pathname.match(/^\/_next\/data\/(.*)(\/docs.*)$/)
     const buildId = match?.[1]
     const slug = match?.[2]
