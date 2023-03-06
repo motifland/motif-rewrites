2427dd9639f3dae14fd23beb6e61109149500b38
Michael Fester <michael.fester@gmail.com>
2023-03-05 19:11:28 -0800

    Fix path

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const MOTIF_PROJECT_URL = 'https://motif-rewrites-docs.vercel.app'
-// const MOTIF_PROJECT_URL = 'http://localhost:3003'
-// const MOTIF_PROJECT_URL = 'http://protestant-tuna-purple.localhost:3001'
+// const MOTIF_PROJECT_URL = 'http://localhost:3001'
 
 export default async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
 
-  if (pathname === '/docs' || pathname.startsWith('/docs/')) {
+  if (pathname.startsWith('/docs')) {
     return NextResponse.rewrite(MOTIF_PROJECT_URL + pathname)
   } else if (pathname.startsWith('/__motif/assets/_next/')) {
     return NextResponse.rewrite(MOTIF_PROJECT_URL + pathname)
