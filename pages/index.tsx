af3bb1f0b348c8f70246b08e26f6feb34f6a9a64
Michael Fester <michael.fester@gmail.com>
2023-03-07 09:26:06 -0800

    Update links

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <>
       <main>
-        <p>Rewrites tests</p>
+        <p>With internal rewrites in external app</p>
+        <p>
+          <Link href="/docs-gsp/page1">With getStaticProps</Link>
+        </p>
+        <p>
+          <Link href="/docs-nogsp/page1">Without getStaticProps</Link>
+        </p>
+        <p>Without internal rewrites in external app</p>
         <p>
           <Link href="/docs-gsp/page1">With getStaticProps</Link>
         </p>
